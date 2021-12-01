dc029a25f5441178df83506a7147139b83434b5a
imadhrit <maharajrabel@gmail.com>
2021-12-01 12:42:09 +0530

    port

---
+++ build/precache-manifest.f6602c7d1eb6e9ec25929b1af4a75655.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab09539a82d26dfafaa76fb208992c9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afa5a6779a16cf4fbc06",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "6d49d3bbf080d34f0f3b",
+    "url": "/static/css/main.d7482e21.chunk.css"
+  },
+  {
+    "revision": "afa5a6779a16cf4fbc06",
+    "url": "/static/js/2.65ed3e8d.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/static/js/2.65ed3e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f96d847ff9009175e7b",
+    "url": "/static/js/3.5cb899cd.chunk.js"
+  },
+  {
+    "revision": "6d49d3bbf080d34f0f3b",
+    "url": "/static/js/main.da9332ed.chunk.js"
+  },
+  {
+    "revision": "fb115daf814dc17e9f13",
+    "url": "/static/js/runtime-main.a03d2dc0.js"
+  }
+]);
